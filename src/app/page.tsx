c2babd5c5744b17e3e99257537f6556f4376e46d
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2025-07-27 12:55:06 +0100

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      hello YT
+      <Button>Shadcn button!</Button>
+    </div>
+  );
+}
